d84fa1d90e125534d0dd510de79e09d48aa5df7b
Johnny Kristensen <johnny.kristensen@akqa.com>
2021-04-03 23:30:35 +0200

    add eb whitelist

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,3 +1,7 @@
-﻿export const WHITELIST_DOMAINS = ["localhost", "ngrok.io"];
+﻿export const WHITELIST_DOMAINS = [
+  "localhost",
+  "ngrok.io",
+  "contentfulmiddlelayer-env.eba-xshmmbcq.eu-central-1.elasticbeanstalk.com",
+];
 
 export const API_KEY_HEADER = "x-mccode-api-key";
